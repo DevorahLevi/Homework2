7ef541d518410cb1d35397502dce8e704105a428
Devorah Levi <devorahlevi1@gmail.com>
2020-02-14 00:07:34 -0600

    Homework 2

--- src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
+++ src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
@@ -4,11 +4,20 @@ import edu.htc.blitstein.caih313.tddstack.IStackable;
 
 public class TddStack {
 
+    static int DEFAULT_DEPTH = 100;
+    long stackDepth;
+
+    public TddStack() {}
+
+    TddStack(long stackDepth) {}
+
     //to compile, this method must return something, but this
     //code is just to make it compile, not a real implementation
     boolean isEmpty(){
         return false;
     }
+
+    boolean isFull() { return false;}
 
     IStackable pop() {
         //any object can be null; again, just to get it to compile
--- src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
+++ src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
@@ -2,6 +2,8 @@ package edu.htc.blitstein.caih313.tddstack.engine;
 
 import edu.htc.blitstein.caih313.tddstack.IStackable;
 import edu.htc.blitstein.caih313.tddstack.resource.campus.Faculty;
+import edu.htc.blitstein.caih313.tddstack.resource.campus.Student;
+import jdk.jfr.StackTrace;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,9 +22,30 @@ public class TddStackTest {
     }
 
     @Test
+    public void testDefaultConstructor()
+    {
+        int expected = TddStack.DEFAULT_DEPTH;
+        assertEquals(expected, testObject.stackDepth);
+    }
+
+    @Test
     public void testNewStackIsEmpty() {
         TddStack tddStack = new TddStack();
         assertTrue(tddStack.isEmpty());
+    }
+
+    @Test
+    public void testNewStackIsFull()
+    {
+        TddStack tddStack = new TddStack();
+        assertFalse(tddStack.isFull());
+    }
+
+    @Test
+    public void testNewStackPop()
+    {
+        TddStack tddStack = new TddStack();
+        assertNull(tddStack.pop());
     }
 
     @Test
@@ -33,5 +56,29 @@ public class TddStackTest {
         tddStack.push(iStackableOrig);
         IStackable iStackablePopped = tddStack.pop();
         assertEquals(iStackableOrig, iStackablePopped);
+    }
+
+    @Test
+    public void testIsEmptyFalseAfterPush()
+    {
+        TddStack tddStack = new TddStack();
+        IStackable object = new Faculty();
+        tddStack.push(object);
+        assertFalse(tddStack.isEmpty());
+    }
+
+    @Test
+    public void testIsFullTrueAfterPushObjects()
+    {
+        TddStack tddStack = new TddStack(3);
+        IStackable object1 = new Faculty();
+        IStackable object2 = new Student();
+        IStackable object3 = new Student();
+
+        tddStack.push(object1);
+        tddStack.push(object2);
+        tddStack.push(object3);
+
+        assertTrue(tddStack.isFull());
     }
 }
